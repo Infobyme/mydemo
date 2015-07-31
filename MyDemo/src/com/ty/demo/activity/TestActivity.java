12af1468c12dbc13990cf99db2fa7240ee178296
Infobyme <tongyang219@163.com>
2015-07-31 14:37:29 +0800

    update TestActivity

--- MyDemo/src/com/ty/demo/activity/TestActivity.java
+++ MyDemo/src/com/ty/demo/activity/TestActivity.java
@@ -1,7 +1,21 @@
 package com.ty.demo.activity;
 
+import android.os.Bundle;
+
+import com.ty.demo.base.BaseActivity;
+
 /**
  * Created by Administrator on 2015/7/31.
  */
-public class TestActivity {
+public class TestActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initView() {
+
+    }
 }
